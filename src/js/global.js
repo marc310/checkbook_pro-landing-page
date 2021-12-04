7e874532f30c058932ca0cc580a7d207e0d254ce
Marcelo Motta <marcelomotta@MacBook-Pro-de-Marcelo.local>
2021-12-04 02:14:44 -0300

    ui updates and structure

--- src/js/global.js
+++ src/js/global.js
@@ -38,9 +38,29 @@ var playVideo = (e)=>{
     let id = e.target.parentNode.id
     let button = document.getElementById(id);
     let video = e.target.parentNode.nextElementSibling.id;
+    let audioBtn = document.getElementById('bt_mute');
     // console.log(e.target.parentNode.nextElementSibling.id)
     button.style.display = 'none';
+    audioBtn.style.opacity = 1;
     document.getElementById(video).play();
+}
+
+const spokeVideo = document.getElementById('spokeVideo');
+var muteVideo = () => {
+  spokeVideo.muted == true ? mute() : unmute();
+}
+
+let mute = () => {
+  audioBtn = document.getElementById('audioBtn');
+  audioBtn.classList.remove('fa-volume-mute')
+  audioBtn.classList.add('fa-volume-up')
+  spokeVideo.muted = false
+}
+let unmute = () => {
+  audioBtn = document.getElementById('audioBtn');
+  audioBtn.classList.add('fa-volume-mute')
+  audioBtn.classList.remove('fa-volume-up')
+  spokeVideo.muted = true
 }
 
 document.getElementById('spokeVideo').addEventListener('ended',showButton,false);
@@ -48,9 +68,5 @@ function showButton() {
     play_button = document.getElementById('spokeVideoPlay');
     play_button.style.display = 'flex';
 }
-document.getElementById('rightToolsVideo').addEventListener('ended',showButton,false);
-function showButton() {
-    play_button = document.getElementById('rightToolsVideoPlay');
-    play_button.style.display = 'flex';
-}
+
 
