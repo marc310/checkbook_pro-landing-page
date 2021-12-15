54a5b5d0af10983864b1539aa7793dc7027df69b
Marcelo Motta <marcelomotta@MacBook-Pro-de-Marcelo.local>
2021-12-15 13:29:26 -0300

    new page landing.html

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', gulp.series(function(){
-    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss', 'src/scss/common/*.scss','src/scss/parts/*.scss'])
     .pipe(sass()) // converte sass em css
     .pipe(gulp.dest('src/css'));
     //
@@ -20,7 +20,7 @@ gulp.task('sass', gulp.series(function(){
 
 // sass watch changes on folder
 gulp.task('watch', gulp.series(function(){
-    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], gulp.parallel(['sass']));
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss', 'src/scss/common/*.scss','src/scss/parts/*.scss'], gulp.parallel(['sass']));
 }));
 
 //tarefa default para executar as tarefas anteriores
